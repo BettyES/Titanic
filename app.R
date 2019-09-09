126209fb69ab9aae0b9c1b147ea84eaa3154fd77
Judit Guimera Busquets <judit.g-busquets@royalmail.com>
2019-09-09 15:41:47 +0100

    Delete the dependency of running the model with test_data attached, so now it can be run only with one line of new data. Stated working on tab Model specification.

--- app.R
+++ app.R
@@ -62,26 +62,35 @@ ui <- navbarPage("Titanic",
                     column(6,
                            h5("And what about the harbour from which passengers embarked?"),
                             plotOutput("embPlot")))    
-  )
+  ),
+  tabPanel("Model Specification",
+    fluidRow(column(8,
+      h5("Does age have something to do with death or survival? And is there a difference between genders?")),
+      column(4,
+        h5("Travelling first class, it would be easier to get to deck. 
+          Could this have had influenced your chances of survival?"))),
+    fluidRow(column(6,
+      h5("As mentioned above, do we see any survival differences between men and women?")),
+      column(6,
+        h5("And what about the harbour from which passengers embarked?"))#,plotOutput("embPlot")))    
+  ))
 )
 
 
 server <- function(input, output) {
   
   runPrediction <- eventReactive(input$do, {
-    df = data.frame("Name" = as.character(input$name),"Age" = as.integer(input$age), "Sex" = as.factor(input$gender), "Embarked" = as.factor(input$port), 
-                    "Pclass" = as.factor(input$class),
-                    "Fare" = as.double(input$fare), "SibSp" = as.integer(input$sibs), "Parch" = as.integer(input$parch))
-    print(df)
-    df = c(test_data,df)
+    df = data.frame("Name" = as.character(input$name),"Age" = as.integer(input$age), 
+      "Sex" = factor(input$gender, levels = c("female", "male")), "Embarked" = factor(input$port, levels = c("C", "Q", "S")), 
+      "Pclass" = factor(input$class, levels = c("1", "2", "3")), "Fare" = as.double(input$fare), 
+      "SibSp" = as.integer(input$sibs), "Parch" = as.integer(input$parch))
     model<-ctree(Survived ~ Pclass+Sex+Age+Embarked+Fare+SibSp+Parch,data=data)
     pred<-predict(model,df)
     pred
   })
   
   output$result <- renderPrint({
-    print(as.numeric(tail(runPrediction(),1)))
-    if(as.numeric(tail(runPrediction(),1))-1 == 1){
+    if(runPrediction() == 1){
       cat("Congratulations!\nYou would have survived the disaster.")
     }else{
       cat("Sorry!\nYou would have not survived.")
@@ -95,25 +104,25 @@ server <- function(input, output) {
       geom_histogram(aes(Age,fill=Survived),color = "white",position="dodge")+facet_wrap(~Sex)
     })
 
-output$classPlot = renderPlot({  
-  ggplot(data)+
-    geom_histogram(aes(Pclass,fill=Survived),color = "white",stat="count",position="dodge")+
-    xlab("Class")
-})
+  output$classPlot = renderPlot({  
+    ggplot(data)+
+      geom_histogram(aes(Pclass,fill=Survived),color = "white",stat="count",position="dodge")+
+      xlab("Class")
+  })
 
-output$sexPlot = renderPlot({  
-  ggplot(data)+
-    geom_histogram(aes(Sex,fill=Survived),color = "white",stat="count",position="dodge")+
-    xlab("Gender")
-})
-
-output$embPlot = renderPlot({ 
-  ggplot(data)+
-    geom_histogram(aes(Embarked,fill=Survived),color = "white",stat="count",position="dodge")+
-    xlab("Harbour")+theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-    scale_x_discrete(breaks=c("Q","S","C"),
-                     labels=c("Queenstown", "Southhampton", "Cherbourg"))
-})
+  output$sexPlot = renderPlot({  
+    ggplot(data)+
+      geom_histogram(aes(Sex,fill=Survived),color = "white",stat="count",position="dodge")+
+      xlab("Gender")
+  })
+  
+  output$embPlot = renderPlot({ 
+    ggplot(data)+
+      geom_histogram(aes(Embarked,fill=Survived),color = "white",stat="count",position="dodge")+
+      xlab("Harbour")+theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+      scale_x_discrete(breaks=c("Q","S","C"),
+        labels=c("Queenstown", "Southhampton", "Cherbourg"))
+  })
   
 }
 
